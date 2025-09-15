f6f4b3caada1c36b2654fe45f8218ba1bf95e926
Jair Ribeiro <jlribeiro@id.uff.br>
2025-09-15 07:05:40 -0300

    hist: Adicionando arquivos para backup de alterações até o momento

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,25 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  // 1. ADICIONE A URL FINAL DO SEU SITE AQUI
+  // Isso força o Astro a criar caminhos absolutos para CSS, JS, etc.
+  site: 'https://jairribeiro.com.br', // SUBSTITUA PELO SEU DOMÍNIO REAL
+
   integrations: [
     tailwind(), 
     icon({
       include: {
-        mdi: ['*'] // Permite o uso de QUALQUER ícone do set 'mdi'
+        mdi: ['*']
       }
     })
-  ]
+  ],
+  
+  // 2. CONFIGURAÇÃO DE i18n LIMPA
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'pt'], // É uma boa prática listar o idioma padrão aqui também
+    routing: {
+      prefixDefaultLocale: false
+    }
+  }
 });
